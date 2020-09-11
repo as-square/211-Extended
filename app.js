6042c1c112d209ba561338aa5f7e9cc0c21a546e
RATED-R-SUNDRAM <ss6437p@gmail.com>
2020-09-11 13:50:57 +0530

    next time

--- app.js
+++ app.js
@@ -18,23 +18,50 @@ var mymodel = mongooose.model("model", new mongooose.Schema({
     message: String
 }))
 app.get("/", (req, res) => {
-    res.render("put")
+    res.render("home")
 
 });
-
 app.post("/daal", (req, res) => {
     var entry = new mymodel({
         name: req.body.name,
         message: req.body.message
     })
     entry.save()
-    res.redirect("/")
+    res.redirect("/contribute")
 });
-app.get("/bf", (req, res) => {
+app.get("/memories", (req, res) => {
     mymodel.find({}, (err, tasks) => {
-        res.render("homes", { array: tasks });
+        res.render("STORIES", { array: tasks });
     });
-})
+});
+app.get("/contact", (req, res) => {
+    res.render("c")
+});
+app.get("/contribute", (req, res) => {
+    res.render("contribute")
+});
+app.get("/akram", (req, res) => {
+    res.render("akram")
+});
+app.get("/aditya", (req, res) => {
+    res.render("aditya")
+});
+app.get("/arjun", (req, res) => {
+    res.render("arjun")
+});
+app.get("/harsh", (req, res) => {
+    res.render("harsh")
+});
+app.get("/abhinay", (req, res) => {
+    res.render("abhinay")
+});
+app.get("/harshit", (req, res) => {
+    res.render("harshit")
+});
+app.get("/shivam", (req, res) => {
+    res.render("shivam")
+});
+
 
 
 
